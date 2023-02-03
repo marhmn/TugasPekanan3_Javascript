ae4f3decf3a9ff378d71c88ada6c326e66851ad5
marhmn <m.a.rahman0505@gmail.com>
2023-02-03 16:15:31 +0700

    perbaikan bug

--- soal1.js
+++ soal1.js
@@ -1,6 +1,6 @@
 const input = require('prompt-sync')({sigint: true});
 var flag1=false;
-var validasiAngka = /^[0-9]+$/;
+var validasiAngka = /^[-+]?[0-9]+$/;
 
 do{
     var nilai = input('Masukan angka: ');
@@ -9,23 +9,20 @@ do{
     }else{
         console.log("harap masukan angka!")
     }
-
 }while(flag1==false)
 
 
 function validasi(x) {
-    if (x%2==0){
-        flag1=true;
-        console.log("akar pangkat 2 dari "+ x +" adalah " + Math.sqrt(x));
-    }else if(x<0){
+    
+    if(x<0){
         console.log("Angka tidak boleh negatif");
         flag1=false;
     }else if(x%2!=0){
         console.log("Angka tidak boleh ganjil");
         flag1=false;
     }else{
-        console.log("tidak boleh kosong");
-        flag1=false;
+        console.log("akar pangkat 2 dari "+ x +" adalah " + Math.sqrt(x));
+        flag1=true;
     }
 
 }
